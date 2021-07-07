5f36274292f29117e05e7445b1d59658b15e2507
Yibo Yan <totoroq@outlook.com>
2021-07-07 20:21:47 +0800

    Finished HomePage

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -19,6 +19,17 @@ module.exports = {
                 't-xl': '0 -20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
                 't-2xl': '0 -25px 50px -12px rgba(0, 0, 0, 0.25)',
                 't-3xl': '0 -35px 60px -15px rgba(0, 0, 0, 0.3)',
+            },
+            backgroundImage: theme => ({
+                'contact-bg': "url('../images/coding.png')",
+                'test-bg': "url('../images/coding.png')",
+            }),
+            backgroundSize: {
+                'auto': 'auto',
+                'cover': 'cover',
+                'contain': 'contain',
+                '50%': '50%',
+                '16': '4rem',
             }
         },
     },
